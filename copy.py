cd482c93e2ee96b052398fd094cab97f4549aede
rookieflip2x <rookieflip2x@gmail.com>
2026-01-22 00:53:55 +0800

    Delete copy.py

--- copy.py
+++
@@ -1,5 +0,0 @@
-CSV_LINKS = {
-    "2023-24": "https://raw.githubusercontent.com/rookieflip2x/workflows/refs/heads/main/NBA_2023-24_%20rookies%20.csv",
-    "2024-25": "https://raw.githubusercontent.com/rookieflip2x/workflows/refs/heads/main/NBA_2024-25_rookies%20.csv",
-    "2025-26": "https://raw.githubusercontent.com/rookieflip2x/workflows/refs/heads/main/NBA_2025-26_rookies%20.csv"
-}
